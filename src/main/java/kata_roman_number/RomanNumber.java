760ee3f758025a699f99aa6e74b1caa02b7a7f03
Somkiat <somkiat.p@gmail.com>
2014-05-01 11:20:42 +0700

    Refactor code to remove duplicate of return statement to variable

--- src/main/java/kata_roman_number/RomanNumber.java
+++ src/main/java/kata_roman_number/RomanNumber.java
@@ -3,8 +3,10 @@ package kata_roman_number;
 public class RomanNumber {
 
 	public static String convert(int number) {
-		if(number == 2) return "II";
-		return "I";
+		StringBuilder result = new StringBuilder();
+		if(number == 2) result.append("I");
+		result.append("I");
+		return result.toString();
 	}
 
 }
