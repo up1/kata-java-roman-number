7648b71a1190ccd198659c12d550bc25b8312a6c
Somkiat <somkiat.p@gmail.com>
2014-05-01 11:49:23 +0700

    Remove duplicate statement with defind array to keep data

--- src/main/java/kata_roman_number/RomanNumber.java
+++ src/main/java/kata_roman_number/RomanNumber.java
@@ -1,14 +1,17 @@
 package kata_roman_number;
 
 public class RomanNumber {
+	
+	static int[] value = {9, 5, 4};
+	static String[] roman = {"IX", "V", "IV"};
 
 	public static String convert(int number) {
 		StringBuilder result = new StringBuilder();
 		int remain = number;
-
-		remain = append(remain, 9, "IX", result);
-		remain = append(remain, 5, "V", result);
-		remain = append(remain, 4, "IV", result);
+		
+		for(int i=0; i<value.length; i++) {
+			remain = append(remain, value[i], roman[i], result);
+		}
 
 		for (int i = 0; i < remain; i++) {
 			result.append("I");
