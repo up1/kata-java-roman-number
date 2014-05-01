8483d9980c0980a1f379c28759846df6d2a3a393
Somkiat <somkiat.p@gmail.com>
2014-05-01 11:24:32 +0700

    Refactor code to remove duplicate of return statement and add if-else

--- src/main/java/kata_roman_number/RomanNumber.java
+++ src/main/java/kata_roman_number/RomanNumber.java
@@ -4,13 +4,13 @@ public class RomanNumber {
 
 	public static String convert(int number) {
 		StringBuilder result = new StringBuilder();
-		
-		if(number == 4) {
-			return "IV";
-		}
-		
-		for(int i=0; i< number; i++) {
-			result.append("I");
+
+		if (number == 4) {
+			result.append("IV");
+		} else {
+			for (int i = 0; i < number; i++) {
+				result.append("I");
+			}
 		}
 		return result.toString();
 	}
