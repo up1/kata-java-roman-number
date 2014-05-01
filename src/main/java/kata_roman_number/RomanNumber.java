e0b314917a07e2b3f4f431714e2f3f0aa2a385cb
Somkiat <somkiat.p@gmail.com>
2014-05-01 11:27:01 +0700

    Add test case 6

--- src/main/java/kata_roman_number/RomanNumber.java
+++ src/main/java/kata_roman_number/RomanNumber.java
@@ -4,7 +4,9 @@ public class RomanNumber {
 
 	public static String convert(int number) {
 		StringBuilder result = new StringBuilder();
-		if (number == 5) {
+		if (number == 6) {
+			result.append("VI");
+		} else if (number == 5) {
 			result.append("V");
 		} else if (number == 4) {
 			result.append("IV");
--- src/test/java/kata_roman_number/RomanNumberTest.java
+++ src/test/java/kata_roman_number/RomanNumberTest.java
@@ -22,5 +22,10 @@ public class RomanNumberTest {
 	public void five() throws Exception {
 		assertEquals("V", RomanNumber.convert(5));
 	}
+	
+	@Test
+	public void six() throws Exception {
+		assertEquals("VI", RomanNumber.convert(6));
+	}
 
 }
