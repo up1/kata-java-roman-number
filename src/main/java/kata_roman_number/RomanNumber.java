107433a9670edd37649eb2471c31c279934730be
Somkiat <somkiat.p@gmail.com>
2014-05-01 11:35:21 +0700

    Refactor to make the same pattern of condition in if-else statement

--- src/main/java/kata_roman_number/RomanNumber.java
+++ src/main/java/kata_roman_number/RomanNumber.java
@@ -5,13 +5,13 @@ public class RomanNumber {
 	public static String convert(int number) {
 		StringBuilder result = new StringBuilder();
 		int remain = number;
-		if (number == 9) {
+		if (number >= 9) {
 			result.append("IX");
 			remain = number - 9;
 		} else if (number >= 5) {
 			result.append("V");
 			remain = number - 5;
-		} else if (number == 4) {
+		} else if (number >= 4) {
 			result.append("IV");
 			remain = number - 4;
 		}
